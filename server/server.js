2fb8826f6ff22bf3e0e84cc805b0feca84e7e521
simonlast <stlast1@gmail.com>
2013-03-14 21:29:54 -0400

    added peer-puzzle, removed comments

--- server/server.js
+++ server/server.js
@@ -1,34 +1,6 @@
 
 var connect = require('connect'),
-	gzip = require('connect-gzip'),
-	fs = require('fs-extra');
-
-var easyimg = require('easyimage');
-
-var resizeImages = function(){
- 	var base = 'site/screens/';
- 	var images = fs.readdirSync(base);
-
- 	fs.removeSync(base + "thumbs");
- 	fs.mkdirsSync(base + "thumbs");
-
- 	for(k in images){
- 		var curr = images[k];
- 		console.log(curr);
- 		if(curr !== 'thumbs' && curr[0] !== '.')
- 		easyimg.resize(
-		  {
-		     src: base + curr, dst: base + 'thumbs/' + curr,
-		     width:375
-		     },
-		  function(err, image) {
-		     console.log(image.name + ' cropped: ' + image.width + ' x ' + image.height);
-		  }
-		);
- 	}
-}
-
-//resizeImages();
+	gzip = require('connect-gzip');
 
 connect.createServer(
   gzip.staticGzip('site')
